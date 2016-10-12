c5db6c2baee1b9f17a192ce5fcb709c710a2c34f
Niklas Brynolf <niklas.brynolf@gmail.com>
2016-10-12 15:22:23 +0200

    fixed null

--- dht_fingers.go
+++ dht_fingers.go
@@ -45,7 +45,7 @@ func (node *DHTNode) updateNetworkFingers() {
 		x, _ := hex.DecodeString(node.nodeId)
 		y, _ := calcFinger(x, (i + 1), bits)
 		booleanResponseTest := false
-		if y == "" {
+		if y == " " {
 			y = "00"
 		} else {
 			responseTimmer := time.NewTimer(time.Second * 3)
