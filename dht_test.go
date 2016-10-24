e62f49b778e5d6587034d1bb71bfeae897014dd1
Niklas Brynolf <niklas.brynolf@gmail.com>
2016-10-24 17:13:54 +0200

    removed comments

--- dht_test.go
+++ dht_test.go
@@ -113,10 +113,3 @@ func TestDHT2(t *testing.T) {
 	time.Sleep(2000 * time.Second)
 
 }
-
-func TestDHT3(t *testing.T) {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
-}
